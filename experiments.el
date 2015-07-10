2bb7171826362bd57820804b05db0fb2cfa2423d
Jay Dixit <sunjaydixit@gmail.com>
2015-07-10 03:14:38 -0400

    deleted old functions

--- experiments.el
+++ experiments.el
@@ -1,58 +1,5 @@
 
 
-(defun erika-send-mail ()
-      "email subtree and HTMLize"
-      (interactive)
-(email-heading)
-(org-mime-htmlize)
-) 
-
- 
-(defun email-heading-to-erika ()
-  "Send the current org-mode heading as the body of an email, with headline as the subject.
-
-use these properties
-TO
-CC
-BCC
-OTHER-HEADERS is an alist specifying additional
-header fields.  Elements look like (HEADER . VALUE) where both
-HEADER and VALUE are strings.
-
-Save when it was sent as a SENT property. this is overwritten on
-subsequent sends."
-  (interactive)
-  ; store location.
-  (setq *email-heading-point* (set-marker (make-marker) (point)))
-  (save-excursion
-    (org-mark-subtree)
-    (let ((content (buffer-substring (point) (mark)))
-	  (TO (org-entry-get (point) "TO" t))
-	  (CC (org-entry-get (point) "CC" t))
-	  (BCC (org-entry-get (point) "BCC" t))
-	  (SUBJECT (nth 4 (org-heading-components)))
-	  (OTHER-HEADERS (eval (org-entry-get (point) "OTHER-HEADERS")))
-	  (continue nil)
-	  (switch-function nil)
-	  (yank-action nil)
-	  (send-actions '((email-send-action . nil)))
-	  (return-action '(email-heading-return)))
-      
-      (compose-mail TO SUBJECT OTHER-HEADERS continue switch-function yank-action send-actions return-action)
-      (message-goto-body)
-      (insert content)
-      (when CC
-	(message-goto-cc)
-	(insert CC))
-      (when BCC
-	(message-goto-bcc)
-	(insert BCC))
-      (if TO
-	  (message-goto-body)
-	(message-goto-to))))) 
-
-
-
 ;; leave an empty line between folded subtrees
 (setq org-cycle-separator-lines 1)
 
