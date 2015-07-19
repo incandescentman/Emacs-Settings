cc0373d214d637a3ef4b3d5f034b81ba78f85f1a
Sunjay E. Dixit <jaydixit.work@gmail.com>
2015-07-19 19:05:12 -0400

    SRC types

--- gnu-emacs-startup.el
+++ gnu-emacs-startup.el
@@ -758,10 +758,7 @@ password: %s" userid password))
   "Insert a `SRC-CODE-TYPE' type source code block in org-mode."
   (interactive
    (let ((src-code-types
-          '("emacs-lisp" "sh" "css"
-"dot"
-
-"latex")))
+          '("emacs-lisp" "sh" "css" "dot" "latex")))
      (list (ido-completing-read "Source code type: " src-code-types))))
   (progn
     (newline-and-indent)
