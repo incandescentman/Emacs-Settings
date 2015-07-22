3c7e203131881711fdcf7748d150138339619ded
Sunjay E. Dixit <jaydixit.work@gmail.com>
2015-07-22 16:37:21 -0400

    debugging yas

--- shared-functions.el
+++ shared-functions.el
@@ -2691,14 +2691,17 @@ Single Capitals as you type."
           (message "Deleted file %s" filename)
           (kill-buffer))))))
 
+   (add-hook 'org-mode-hook
+                    (lambda ()
                       (org-set-local 'yas-trigger-key [tab])
-                      (define-key yas-keymap [tab] 'yas-next-field-or-maybe-expand) 
-
+                      (define-key yas-keymap [tab] 'yas-next-field-or-maybe-expand))) 
 
     (defun yas-org-very-safe-expand ()
             (let ((yas-fallback-behavior 'return-nil)) (yas-expand))) 
 
+    (add-hook 'org-mode-hook
+                    (lambda ()
                       (make-variable-buffer-local 'yas-trigger-key)
                       (setq yas-trigger-key [tab])
                       (add-to-list 'org-tab-first-hook 'yas-org-very-safe-expand)
-                      (define-key yas-keymap [tab] 'yas-next-field) 
+                      (define-key yas-keymap [tab] 'yas-next-field))) 
