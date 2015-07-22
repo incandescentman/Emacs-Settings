7894422a3557ea38709e312a82728e0e2572915b
Sunjay E. Dixit <jaydixit.work@gmail.com>
2015-07-22 19:31:48 -0400

    YAS + autocomplete

--- shared-functions.el
+++ shared-functions.el
@@ -2728,6 +2728,7 @@ Single Capitals as you type."
           (kill-buffer))))))
 
 (require 'yasnippet) 
+(yas-global-mode 1) 
 
 ;; load yasnippet directories
 ;; TODO needs to be set before loading snippets somehow
@@ -2737,6 +2738,19 @@ Single Capitals as you type."
 ;; don't insert random spaces in my prose
 (setq yas-indent-line (quote none)) 
 
+;; take input word including hyphen.
+(setq yas/key-syntaxes '("w_" "w_." "^ ")) ; default is '("w" "w_" "w_." "^ ")
+
+;;; auto complete mod
+;;; should be loaded after yasnippet so that they can work together
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;;; set the trigger key so that it can work together with yasnippet on tab key,
+;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>") 
 
 
    (add-hook 'org-mode-hook
