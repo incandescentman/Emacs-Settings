399a85f171c5e31d877754ce0767165cfe37a048
Sunjay E. Dixit <jaydixit.work@gmail.com>
2015-08-07 17:30:26 -0400

    hl-line mode

--- custom.el
+++ custom.el
@@ -385,7 +385,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-line ((t (:background "#FBFFAD" :foreground "black"))))
+ '(hl-line ((t (:foreground "black"))))
  '(org-headline-done ((t (:strike-through t))))
  '(org-link ((t (:inherit link :underline nil))))
  '(tabula-rasa-cursor ((t (:inherit nil :foreground "red" :inverse-video t))) t t)
