969e58290f381923fc4988fdf6741c8745e7b0d0
Sunjay E. Dixit <jaydixit.work@gmail.com>
2015-09-16 13:57:53 -0400

    additions to erika-send-email-styled

--- custom.el
+++ custom.el
@@ -31,8 +31,6 @@
  '(ac-auto-show-menu 2.0)
  '(ac-auto-start 4)
  '(ac-candidate-menu-min 3)
- '(ansi-color-names-vector
-   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(auto-save-visited-file-name t)
  '(autopair-blink t)
  '(autopair-global-mode t)
@@ -79,7 +77,6 @@
  '(edit-server-new-frame t)
  '(eshell-load-hook (quote ((lambda nil (abbrev-mode -1)))))
  '(fasd-enable-initial-prompt nil)
- '(fci-rule-color "#383838")
  '(flyspell-abbrev-p t)
  '(flyspell-mark-duplications-exceptions
    (quote
@@ -146,7 +143,9 @@
      ("\\.\\(?:jp?g\\|png\\)\\'" "display"
       (file)))))
  '(org-M-RET-may-split-line (quote ((item . t))))
- '(org-agenda-files nil)
+ '(org-agenda-files
+   (quote
+    ("~/github/incandescentman.github.io/org-agenda-test-file.org")))
  '(org-agenda-jump-prefer-future t)
  '(org-agenda-prefix-format
    (quote
@@ -336,11 +335,7 @@
  '(rm-blacklist
    (quote
     (" hl-p" "Guide" "Olv" "Helm" "Palimpsest" "Olivetti")))
- '(safe-local-variable-values
-   (quote
-    ((buffer-file-coding-system . utf-8-unix)
-     (org-html-head-include-scripts)
-     (org-export-allow-bind-keywords . t))))
+ '(safe-local-variable-values (quote ((org-export-allow-bind-keywords . t))))
  '(send-mail-function (quote sendmail-send-it))
  '(smex-prompt-string "I love you. ")
  '(sp-base-key-bindings nil)
@@ -351,28 +346,6 @@
  '(undo-limit 800000)
  '(user-full-name "Jay Dixit")
  '(user-mail-address "dixit@aya.yale.edu")
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3")
  '(visual-line-mode nil t)
  '(web-mode-load-hook (quote ((lambda nil (abbrev-mode -1)))))
  '(writeroom-global-effects
@@ -388,9 +361,6 @@
  '(notmuch-search-matching-authors ((t (:inherit default :height 1.2))))
  '(notmuch-search-subject ((t (:inherit default :height 1.2))))
  '(notmuch-tag-face ((t (:foreground "OliveDrab1" :height 1))))
- '(org-ellipsis ((t (:foreground "violet" :underline t))))
- '(org-headline-done ((t (:strike-through t))))
- '(org-link ((t (:inherit link :underline nil))))
  '(tabula-rasa-cursor ((t (:inherit nil :foreground "red" :inverse-video t))) t t)
  '(writegood-weasels-face ((t (:underline (:color "orange" :style wave))))))
 
