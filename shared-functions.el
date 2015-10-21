3a5ea7a93d94a8a1c4dbee266f164d0fe375b244
Sunjay E. Dixit <jaydixit.work@gmail.com>
2015-10-20 23:52:03 -0400

    good

--- shared-functions.el
+++ shared-functions.el
@@ -7,6 +7,7 @@
 (add-to-list 'package-archives
              '("marmalade" .
                "http://marmalade-repo.org/packages/" ))
+
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
