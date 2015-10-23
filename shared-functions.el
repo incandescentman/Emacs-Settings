698b498e759fdd5d2a4bb4d7bfae44660a1453f3
Sunjay E. Dixit <jaydixit.work@gmail.com>
2015-10-23 02:17:40 -0400

    multiple cursors?

--- shared-functions.el
+++ shared-functions.el
@@ -3479,6 +3479,50 @@ event of an error or nonlocal exit."
 (define-key key-minor-mode-map (kbd "M-s-k") 'org-cut-subtree)
 (define-key key-minor-mode-map (kbd "C-s-k") 'org-cut-subtree)
 
+(require 'multiple-cursors-core)
+;; This is globally useful, so it goes under `C-x', and `m'
+;; for "multiple-cursors" is easy to remember.
+(define-key ctl-x-map "\C-m" #'mc/mark-all-dwim)
+;; Usually, both `C-x C-m' and `C-x RET' invoke the
+;; `mule-keymap', but that's a waste of keys. Here we put it
+;; _just_ under `C-x RET'.
+(define-key ctl-x-map (kbd "<return>") mule-keymap)
+
+;; Remember `er/expand-region' is bound to M-2!
+;; (define-key key-minor-mode-map (kbd "M-#") 'mc/mark-all-dwim)
+(global-set-key (kbd "M-2") #'mc/mark-all-dwim)
+;; (global-set-key (kbd "M-3") #'mc/mark-next-like-this)
+;; (global-set-key (kbd "M-4") #'mc/mark-previous-like-this)
+(define-key key-minor-mode-map (kbd "M-3") 'mc/mark-next-like-this)
+(define-key key-minor-mode-map (kbd "M-4") 'mc/mark-previous-like-this)
+
+
+;; These vary between keyboards. They're supposed to be
+;; Shifted versions of the two above.
+(global-set-key (kbd "M-£") #'mc/unmark-next-like-this)
+(global-set-key (kbd "M-$") #'mc/unmark-previous-like-this)
+
+(define-prefix-command 'endless/mc-map)
+;; C-x m is usually `compose-mail'. Bind it to something
+;; else if you use this command.
+(define-key ctl-x-map "m" 'endless/mc-map)
+
+;;; Really really nice!
+(define-key endless/mc-map "i" #'mc/insert-numbers)
+(define-key endless/mc-map "h" #'mc-hide-unmatched-lines-mode)
+(define-key endless/mc-map "a" #'mc/mark-all-like-this)
+
+;;; Occasionally useful
+(define-key endless/mc-map "d"
+  #'mc/mark-all-symbols-like-this-in-defun)
+(define-key endless/mc-map "r" #'mc/reverse-regions)
+(define-key endless/mc-map "s" #'mc/sort-regions)
+(define-key endless/mc-map "l" #'mc/edit-lines)
+(define-key endless/mc-map "\C-a"
+  #'mc/edit-beginnings-of-lines)
+(define-key endless/mc-map "\C-e"
+  #'mc/edit-ends-of-lines)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; swiper                                                                 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -3487,8 +3531,6 @@ event of an error or nonlocal exit."
   :type 'integer) 
 (global-set-key (kbd "C-s") 'swiper)
 (setq ivy-display-style 'fancy)
-
-(define-key key-minor-mode-map (kbd "M-m") 'swiper-mc)
 
 ;; (require 'wrap-region)
 ;; (wrap-region-add-wrapper "*" "*" "*")  
