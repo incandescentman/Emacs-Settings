a086dc1354756e1178343c7e6773bb4ac53d8c94
Sunjay E. Dixit <jaydixit.work@gmail.com>
2015-11-08 01:03:48 -0500

    hydra experiments

--- shared-functions.el
+++ shared-functions.el
@@ -3693,11 +3693,6 @@ The full path into relative path and insert it as a local file link in org-mode"
 (dotimes (n 10)
   (global-unset-key (kbd (format "M-%d" n))))
 
-(defhydra hydra-zoom (global-map "{")
-  "zoom"
-  ("x" text-scale-increase "in")
-  ("l" text-scale-decrease "out"))
-
 (defun refile-region-or-subtree (beg end copy)
 (interactive "P")
 (if
@@ -3707,3 +3702,85 @@ The full path into relative path and insert it as a local file link in org-mode"
     (org-refile); else
 )
 )
+
+(defhydra hydra-zoom (global-map "s-n")
+  "email"
+  ("ek" erika-send-mail "erika-send-mail")
+  ("nm" notmuch "notmuch")
+)
+
+(global-set-key
+ (kbd "C-n")
+(defhydra hydra-move
+   (:body-pre (next-line))
+   "move"
+   ("n" next-line)
+   ("p" previous-line)
+   ("f" forward-char)
+   ("b" backward-char)
+   ("a" beginning-of-line)
+   ("e" move-end-of-line)
+   ("v" scroll-up-command)
+   ;; Converting M-v to V here by analogy.
+   ("V" scroll-down-command)
+   ("l" recenter-top-bottom)))
+
+(defhydra hydra-helm (:hint nil :color pink)
+	"
+									  ╭──────┐
+   Navigation   Other  Sources     Mark             Do             Help   │ Helm │
+  ╭───────────────────────────────────────────────────────────────────────┴──────╯
+	^_k_^         _K_       _p_   [_m_] mark         [_v_] view         [_H_] helm help
+	^^↑^^         ^↑^       ^↑^   [_t_] toggle all   [_d_] delete       [_s_] source help
+    _h_ ←   → _l_     _c_       ^ ^   [_u_] unmark all   [_f_] follow: %(helm-attr 'follow)
+	^^↓^^         ^↓^       ^↓^    ^ ^               [_y_] yank selection
+	^_j_^         _J_       _n_    ^ ^               [_w_] toggle windows
+  --------------------------------------------------------------------------------
+	"
+	("<tab>" helm-keyboard-quit "back" :exit t)
+	("<escape>" nil "quit")
+	("\\" (insert "\\") "\\" :color blue)
+	("h" helm-beginning-of-buffer)
+	("j" helm-next-line)
+	("k" helm-previous-line)
+	("l" helm-end-of-buffer)
+	("g" helm-beginning-of-buffer)
+	("G" helm-end-of-buffer)
+	("n" helm-next-source)
+	("p" helm-previous-source)
+	("K" helm-scroll-other-window-down)
+	("J" helm-scroll-other-window)
+	("c" helm-recenter-top-bottom-other-window)
+	("m" helm-toggle-visible-mark)
+	("t" helm-toggle-all-marks)
+	("u" helm-unmark-all)
+	("H" helm-help)
+	("s" helm-buffer-help)
+	("v" helm-execute-persistent-action)
+	("d" helm-persistent-delete-marked)
+	("y" helm-yank-selection)
+	("w" helm-toggle-resplit-and-swap-windows)
+	("f" helm-follow-mode))
+
+(global-set-key
+ (kbd "C-9")
+
+(defhydra multiple-cursors-hydra (:hint nil)
+  "
+     ^Up^            ^Down^        ^Other^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
+^ ^             ^ ^             [_q_] Quit
+"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("r" mc/mark-all-in-region-regexp :exit t)
+  ("q" nil)) )
