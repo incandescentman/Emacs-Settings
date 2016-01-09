68eb15a8df28117d2ea25756cfcc9f29ce002eb9
Sunjay E. Dixit <jaydixit.work@gmail.com>
2016-01-09 18:34:23 -0500

    searchlink

--- gnu-emacs-startup.el
+++ gnu-emacs-startup.el
@@ -279,6 +279,8 @@
 ;; and the keybindings
 ;; mk - mykeybindings
 
+(define-key key-minor-mode-map (kbd "s-.") 'searchlink)
+
 (define-key key-minor-mode-map (kbd "s-m c s") 'org-clone-subtree)
 
 ;; (define-key key-minor-mode-map (kbd "s-p") 'refile-region)
