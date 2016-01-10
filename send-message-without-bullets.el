d78100c17d713acce5740b172ac6671ba4e289db
Klimenko Serj <klimenko.serj@gmail.com>
2016-01-10 22:41:41 +0200

    add:save-buffer-to-sent-emails-org-file

--- send-message-without-bullets.el
+++ send-message-without-bullets.el
@@ -1,6 +1,18 @@
+(defvar *sent-emails-org-file* "~/sent-emails.org")
+(defun save-buffer-to-sent-emails-org-file ()
+  ;; header
+  (write-region
+   (concat "\n\n\n* ===============================\n* -- "
+           (current-time-string)
+           " --\n* -------------------------------\n\n")
+   0 *sent-emails-org-file* t)
+  ;; buffer
+  (write-region nil 0 *sent-emails-org-file* t))
+
 (defun send-message-without-bullets ()
   (interactive)
   (remove-hook 'org-mode-hook 'org-bullets-mode)
+  (save-buffer-to-sent-emails-org-file)
   (message-send)
   (add-hook 'org-mode-hook 'org-bullets-mode))
 
