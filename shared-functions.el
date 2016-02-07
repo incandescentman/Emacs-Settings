62fa3625ed092be958b6c386bde0f8c13744b9cf
Sunjay E. Dixit <jaydixit.work@gmail.com>
2016-02-07 13:15:05 -0500

    trying to fontify QUESTIONS... doesn't work yet

--- shared-functions.el
+++ shared-functions.el
@@ -3951,7 +3951,9 @@ The full path into relative path and insert it as a local file link in org-mode"
   "fontify list titles")
 
 (font-lock-add-keywords 'org-mode
-      '(("^.*:[ ]*$" . 'list-title-face)))
+      '(("^.*:[ ]*$" . 'list-title-face))
+                              '("^QUESTION:" . 'list-title-face) 
+)
 
 (defun downcase-or-endless-downcase ()
 (interactive)
