a24755a99aee2392c2f4573c09b3e463798fb964
Sunjay E. Dixit <jaydixit.work@gmail.com>
2016-04-04 23:31:15 -0400

    removed the quote

--- shared-functions.el
+++ shared-functions.el
@@ -3277,7 +3277,7 @@ Single Capitals as you type."
    (mu4e-message-contact-field-matches msg 
        :to "dixit@aya.yale.edu")))
  :vars '
-((user-mail-address . '("dixit@aya.yale.edu"))
+((user-mail-address . ("dixit@aya.yale.edu"))
   (user-full-name . "Jay Dixit" )
   (mu4e-maildir . "/Users/jay/Dropbox/mail/gmail") 
   (mu4e-mu-home . "/Users/jay/Dropbox/mail/mu/gmail") 
@@ -3294,7 +3294,7 @@ Single Capitals as you type."
 		 (when msg 
 		 (mu4e-message-contact-field-matches msg 
 		 :to "jay@vivovii.com")))
-	 :vars '((user-mail-address . '("jay@vivovii.com"))
+	 :vars '((user-mail-address . ("jay@vivovii.com"))
   (user-full-name . "Jay Dixit" )
   (mu4e-maildir . "/Users/jay/Dropbox/mail/vivovii") 
   (mu4e-mu-home . "/Users/jay/Dropbox/mail/mu/vivovii") 
