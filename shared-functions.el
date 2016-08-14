74c2a6d4e9ff7877da7648800e5a608475cc4a16
Sunjay E. Dixit <jaydixit.work@gmail.com>
2016-08-14 14:02:53 -0400

    fixed

--- shared-functions.el
+++ shared-functions.el
@@ -3758,7 +3758,7 @@ already narrowed."
 '("l" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC" "<src lang=\"emacs-lisp\">\n?\n</src>"))
 
 (add-to-list 'org-structure-template-alist
-'("le" "#+BEGIN_EXAMPLE emacs-lisp\n?\n#+END_SRC" "<src lang=\"emacs-lisp\">\n?\n</src>"))
+'("le" "#+BEGIN_EXAMPLE emacs-lisp\n?\n#+END_EXAMPLE" "<src lang=\"emacs-lisp\">\n?\n</src>"))
 
 ;; (add-hook 'org-mode-hook '(lambda () '(element-debug-mode 1)))
 
