6a44b1a476f115604a9f609d083bf431bb6dcab1
Sunjay E. Dixit <jaydixit.work@gmail.com>
2016-09-05 05:56:50 -0400

    moving out extraneous files

--- zone-matrix/example_user_config.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'load-path (concat dotfiles-dir "zone-matrix"))
-(require 'zone-matrix)
-(require 'zone-matrix-settings)
-(require 'zone-settings)
-
-(setq zone-programs [zone-matrix])
-(zone-when-idle 60)
