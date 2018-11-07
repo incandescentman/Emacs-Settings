df6f68e4666474bdffaf41ac6d99866182520925
Jay Dixit <jaydixit.work@gmail.com>
2018-11-07 01:01:16 -0500

    latex image width

--- new-latex-templates/elegant-garamond.el
+++ new-latex-templates/elegant-garamond.el
@@ -329,7 +329,6 @@
 
 
 
-
 \\usepackage[breaklinks=true,linktocpage,xetex]{hyperref}
 \\hypersetup{colorlinks, citecolor=elegantblue,filecolor=elegantblue,linkcolor=elegantblue,urlcolor=elegantblue}
 
