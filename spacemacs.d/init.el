e287ef5410aed7f0cb29208ee6bf459ce3a1c7fd
Jay Dixit <jaydixit.work@gmail.com>
2019-02-22 15:57:47 -0500

    good

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -933,13 +933,14 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(beacon ((t (:inherit highlight))))
+ '(bold ((t (:inherit font-lock-warning-face :weight bold))))
+ '(zz-fringe-for-narrowing ((t (:background "white"))))
+  '(beacon ((t (:inherit highlight))))
  '(bold ((t (:inherit font-lock-warning-face :foreground "Red" :weight bold :height 1.1 :family "Sans Serif"))))
  '(flyspell-duplicate ((t (:underline (:style wave :color "red")))))
  '(flyspell-incorrect ((t (:underline (:style wave :color "red")))))
  '(italic ((t (:foreground "DarkViolet" :slant italic :height 1.4 :family "Garamond"))))
  '(org-drill-visible-cloze-face ((t (:background "#FFFFD7" :foreground "black"))))
- '(zz-fringe-for-narrowing ((t nil)))
  '(org-level-1 ((t (:family "Gotham Narrow" :weight bold))))
  '(org-link ((t (:underline nil :inherit link))))
  '(org-quote ((t (:inherit default :background "#FFFFE0" :foreground "dim gray" :slant normal)))))
