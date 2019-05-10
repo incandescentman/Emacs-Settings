c626c1f891c71c77e74d95b10ee970a6e0cfb243
Jay Dixit <jaydixit.work@gmail.com>
2019-05-10 00:38:40 -0700

    no orgalist

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -32,6 +32,7 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     python
      graphviz
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
