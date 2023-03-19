3ca106ef522895f154fd9a920995ba5175f828a0
Sunjay E. Dixit <jaydixit.work@gmail.com>
2023-03-19 00:08:14 -0500

    fixed org-roam keybindings

--- org-roam-config.el
+++ org-roam-config.el
@@ -80,9 +80,12 @@
    ;; ("C-c r d s" . org-schedule)
 ;; ("s-u t" . org-roam-dailies-goto-today)
 ("s-u t" . org-roam-dailies-capture-today)
-("s-<left>" . org-roam-dailies-capture-today)
-("s-<right>" . org-roam-node-find)
+("S-s-<left>" . org-roam-dailies-capture-today)
+("S-s-<right>" . org-roam-node-find)
 ("s-u d" . org-roam-dailies-goto-date)
+
+(":" . insert-colon)
+
    ))
 
 
