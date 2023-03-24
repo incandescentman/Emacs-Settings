78fdf154b301e47987b3a227de7617c406000025
Sunjay E. Dixit <jaydixit.work@gmail.com>
2023-03-24 18:17:50 -0400

    multiple cursors layer

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -102,6 +102,7 @@ This function should only modify configuration layer settings."
      compleseus
      ;; lsp
      ;; markdown
+     multiple-cursors
      ;; python
 
      ;; i commented out the part below so that spacemacs
@@ -1566,7 +1567,7 @@ It should only modify the values of Spacemacs settings."
    ;; refer to the DOCUMENTATION.org for more info on how to create your own
    ;; spaceline theme. Value can be a symbol or list with additional properties.
    ;; (default '(spacemacs :separator wave :separator-scale 1.5))
-   ;; dotspacemacs-mode-line-theme '(spacemacs :separator wave :separator-scale 1.5)
+   dotspacemacs-mode-line-theme '(spacemacs :separator wave :separator-scale 1.5)
 
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    ;; (default t)
