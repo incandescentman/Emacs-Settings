3b5eb53559a4ef22ced32c3713382c422a1ad598
Sunjay E. Dixit <jaydixit.work@gmail.com>
2023-03-30 01:39:59 -0400

    working

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -330,7 +330,7 @@ consult-projectile ;; this finds filenames only. it does not search full text.
    ;; installs only the used packages but won't delete unused ones. `all'
    ;; installs *all* packages supported by Spacemacs and never uninstalls them.
    ;; (default is `used-only')
-   dotspacemacs-install-packages 'used-but-keep-unused))
+   dotspacemacs-install-packages 'used-only))
 
 (defun dotspacemacs/init ()
   "Initialization:
