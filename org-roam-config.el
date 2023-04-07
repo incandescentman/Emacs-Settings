f24bb96a622e0bda228e210227fd2a23c55f9d41
Sunjay E. Dixit <jaydixit.work@gmail.com>
2023-04-07 12:07:22 -0400

    Update org-roam-config.el

--- org-roam-config.el
+++ org-roam-config.el
@@ -82,6 +82,7 @@
           ))
   :bind
   (
+   ;; org-roam keybindings
    ("s-u f" . org-roam-find-node)
    ("S-s-<up>" . org-roam-backlinks-buffer)
    ;; ("S-s-<down>" . projectile-ripgrep)
@@ -90,47 +91,42 @@
    ("s-u l" . org-roam-buffer-toggle)
    ("s-u i" . org-roam-node-insert)
    ("s-u c" . org-roam-capture)
-   ;; ("C-c r d a" . org-agenda)
-   ;; ("C-c r d s" . org-schedule)
-   ("s-j" . org-roam-dailies-goto-today)
-   ("C-S-d" . org-roam-dailies-goto-today)
    ("S-s-<left>" .  org-roam-node-insert)
    ("S-s-<right>" . org-roam-node-find)
-   ("s-u d" . org-roam-dailies-goto-date)
+   ("s-u r" . org-roam-refile)
 
+   ;; goto
+   ("s-u o" . org-roam-dailies-find-date)
+   ("s-u d" . org-roam-dailies-goto-date)
+   ("s-u p" . org-roam-dailies-goto-previous-note)
+   ("s-u n" . org-roam-dailies-goto-next-note)
+   ("s-j"   . org-roam-dailies-goto-today)
+   ("C-S-d" . org-roam-dailies-goto-today)
+   ("s-u y" . org-roam-dailies-goto-yesterday)
+   ("s-u T" . org-roam-dailies-goto-tomorrow)
+   ("s-u Y" . org-roam-dailies-yesterday)
+
+   ;; capture
+   ("s-u t" . org-roam-dailies-capture-today)
+   ("s-u c" . org-roam-dailies-capture-today)
+   ("s-u k" . org-roam-dailies-capture-date)
+
+   ;; search
+   ("s-/ sn" . org-roam-search-nodes)
    (":" . insert-colon)
 
    ("s-u h" . org-roam-heading-add) ;; org-roam create heading
-
-   ("s-u t" . org-roam-dailies-capture-today)
-   ("s-u Y" . org-roam-dailies-yesterday)
-   ("s-u y" . org-roam-dailies-goto-yesterday)
-   ("s-u T" . org-roam-dailies-goto-tomorrow)
-   ("s-u c" . org-roam-dailies-capture-today)
-   ("s-u r" . org-roam-refile)
    ("s-u a" . org-roam-alias-add)
-   ("s-u o" . org-roam-dailies-find-date)
-   ("s-u p" . org-roam-dailies-goto-previous-note)
-   ("s-u n" . org-roam-dailies-goto-next-note)
-   ("s-u k" . org-roam-dailies-capture-date)
-   ("s-u m" . org-roam-dailies-goto-date)
+
    ;;   ("s-T" . org-roam-tags)
 
+   ;; ("C-c r d a" . org-agenda)
+   ;; ("C-c r d s" . org-schedule)
 
-   ("s-/ sn" . org-roam-search-nodes)
-                                        ;   ("s-/ st" . consult-)
-                                        ; ("s-/ l" . council-)
-
-   ("s-/ dg" . deadgrep) ; not incremental. but nicely formatted
-   ("s-/ cp" . counsel-projectile-ag) ; as an alternative to deadgrep check out ag so maybe it's better
-   ("s-/ rg" . consult-ripgrep) ; pretty slick, shows you the actual file context
-   ("s-/ gg" . consult-git-grep) ; pretty great, like projectile, doesn't respect .projectile
    ))
 
 ;; (global-page-break-lines-mode 0)
 (advice-add #'org-roam-fontify-like-in-org-mode :around (lambda (fn &rest args) (save-excursion (apply fn args))))
-
-
 
 (setq org-roam-completion-everywhere t)
 ;; doesn't work for some reason
@@ -139,14 +135,11 @@
 ;; source: [[https://github.com/org-roam/org-roam/issues/1732][clicking on any link within *org-roam* buffer fails with an error message · Issue #1732 · org-roam/org-roam]]
 ;; if necessary, consider using org-roam-buffer-refresh
 
-
-
 (defun org-roam-backlinks-buffer ()
   (interactive)
   (org-roam-buffer-toggle)
   (other-window 1)
   )
-
 
 (defun org-roam-search-nodes ()
   "Search org-roam directory using consult-ripgrep. With live-preview."
@@ -158,6 +151,15 @@
 (defalias 'org-roam-heading-add 'org-id-get-create)
 (defalias 'org-roam-find-node 'org-roam-node-find)
 (defalias 'org-roam-insert-node 'org-roam-node-insert)
+
+; Global keybindings not related to org-roam
+(global-set-key (kbd "s-/ dg") 'deadgrep) ; not incremental. but nicely formatted
+(global-set-key (kbd "s-/ cp") 'counsel-projectile-ag) ; as an alternative to deadgrep check out ag so maybe it's better
+(global-set-key (kbd "s-/ rg") 'consult-ripgrep) ; pretty slick, shows you the actual file context
+(global-set-key (kbd "s-/ gg") 'consult-git-grep) ; pretty great, like projectile, doesn't respect .projectile
+
+(global-set-key (kbd "s-/ st") 'consult-) ;
+(global-set-key (kbd "s-/ l") 'counsel-) ;
 
 (use-package org-roam-protocol
   :after org-roam)
