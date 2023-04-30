aa1a361c0e662c02af6a89e9ee15d93e2c9a772d
Sunjay E. Dixit <jaydixit.work@gmail.com>
2023-04-29 22:41:47 -0400

    consult-ripgrep-current-directory

--- search-commands.el
+++ search-commands.el
@@ -1,9 +1,10 @@
 (define-key key-minor-mode-map (kbd "s-/ gr") 'consult-grep)
 
 ;; Search
-(global-set-key (kbd "s-/ dg") 'deadgrep) ; not incremental. but nicely formatted. lays it all out nicely in a buffer.
+(global-set-key (kbd "s-/ dg") 'deadgrep-current-directory) ; not incremental. but nicely formatted. lays it all out nicely in a buffer.
 (global-set-key (kbd "s-/ pa") 'counsel-projectile-ag) ; as an alternative to deadgrep check out ag so maybe it's better
-(global-set-key (kbd "s-/ rg") 'consult-ripgrep) ; pretty slick, shows you the actual file context
+(global-set-key (kbd "s-/ rg") 'consult-ripgrep-current-directory) ; pretty slick, shows you the actual file context
+(global-set-key (kbd "s-k rg") 'consult-ripgrep-current-directory) ; pretty slick, shows you the actual file context
 (global-set-key (kbd "s-/ gg") 'consult-git-grep) ; pretty great, like projectile, doesn't respect .projectile
 
 ;; (global-set-key (kbd "s-/ st") 'consult-) ;
@@ -21,3 +22,17 @@
  "Do counsel-ag on the org roam directory"
  (interactive)
  (counsel-ag nil org-roam-directory))
+
+(defun consult-ripgrep-current-directory ()
+ "Search current directory using consult-ripgrep."
+ (interactive)
+ (consult-ripgrep default-directory))
+
+
+(defun deadgrep-current-directory ()
+ "Search current directory using deadgrep."
+ (interactive)
+ (let ((search-term (read-string "Search term: ")))
+  (deadgrep search-term default-directory)))
+
+(defalias 'deadgrep-project-directory 'deadgrep)
