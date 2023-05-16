99db952dfc19d51cc635f1ee507a21ce131e59a6
Sunjay E. Dixit <jaydixit.work@gmail.com>
2023-05-15 21:46:48 -0400

    reveal-in-finder

--- org-roam-config.el
+++ org-roam-config.el
@@ -34,6 +34,7 @@
 * export settings                       :ARCHIVE:noexport:
 #+OPTIONS: H:6 num:nil toc:nil :nil @:t ::t |:t ^:t -:t f:t *:t <:t \n:t
 #+HTML_HEAD: <link rel='stylesheet' type='text/css' href='https://incandescentman.github.io/css/cortex-theme.css' />
+
 * ${title}
 - %?"
 	         :target (file+head "notes/%<%Y%m%d%H%M%S>-${slug}.org"
