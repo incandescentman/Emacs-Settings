0d12e686695dafd717266a29c219a46314c59ae8
Sunjay E. Dixit <jaydixit.work@gmail.com>
2023-10-15 16:27:47 -0400

    email working. (but but not CSS)

--- spacemacs-new-config.el
+++ spacemacs-new-config.el
@@ -90,6 +90,7 @@
 (org-babel-load-file "/Users/jay/emacs/emacs-settings/fonts-and-themes.org")
 (load "/Users/jay/emacs/archive/email.el")
 (org-babel-load-file "/Users/jay/emacs/emacs-settings/email-sending.org")
+(org-babel-load-file "/Users/jay/emacs/external-packages/org-mime-stuff/org-mime-stuff.org")
 (load "/Users/jay/emacs/prelude/core/prelude-core.el")
 ;;  (load "/Users/jay/emacs/emacs-settings/skeletons.el")
 (load "/Users/jay/emacs/emacs-settings/prelude-key-chord.el")
