a5341eb5117847a2edd639cffe7687ded4ac58a7
Sunjay E. Dixit <jaydixit.work@gmail.com>
2024-04-24 11:26:48 -0700

    adjustments from work laptop for compatibility

--- jay-osx.el
+++ jay-osx.el
@@ -16,8 +16,8 @@
 
 
 
-(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
-  (normal-top-level-add-subdirs-to-load-path))
+;;(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+ ;; (normal-top-level-add-subdirs-to-load-path))
 
 
 (setq mac-command-key-is-meta nil)
