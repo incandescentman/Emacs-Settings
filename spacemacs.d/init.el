51605fa3011d965ba6cf5dc33d2087e662025d3f
Sunjay E. Dixit <jaydixit.work@gmail.com>
2024-08-23 00:02:35 -0400

    Update init.el

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -88,7 +88,7 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(
+   '(python
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
