aad227ce97fada910a1179b07245e424a4a09cc5
Sunjay E. Dixit <jaydixit.work@gmail.com>
2024-10-14 12:42:13 -0400

    org-roam-dailies-goto-previous-note

--- org-roam-config.el
+++ org-roam-config.el
@@ -86,7 +86,7 @@ If the point is already on an existing link, it prepends ', ' before inserting t
   (defun org-roam-yesterday ()
     (interactive)
     (jay/save-some-buffers)
-    (org-roam-dailies-yesterday)
+    (org-roam-dailies-goto-previous-note)
     )
 
   ;; Add custom functions and advice
