75a730537001ecfbe507092d741fff2b509e9668
Jay Dixit <jaydixit.work@gmail.com>
2025-05-30 14:25:17 -0400

    Revert "Merge branch 'master' into main"
    This reverts commit 9332883a242f5947307ecdd6d8e8488b05149aa0, reversing
    changes made to 55319370313599687b86b8fa48c2109d99d32f70.

--- spacemacs-new-config.el
+++ spacemacs-new-config.el
@@ -1,9 +1,44 @@
+;; ------------- init.el (or early-load file) --------------------------
+(require 'ob-tangle)          ; 1️⃣ make sure tangling is available
+
+(with-eval-after-load 'org    ; 2️⃣ define + register the helper
+  (defun jd/org-auto-tangle ()
+    "Tangle this Org buffer on save when it has #+auto_tangle: t."
+    (when (and (derived-mode-p 'org-mode)
+               (member "t"
+                       (cdr (assoc "auto_tangle"
+                                   (org-collect-keywords '("auto_tangle"))))))
+      (org-babel-tangle)))
+
+  ;; Global hook is fine; predicate prevents work on non-Org files.
+  (add-hook 'after-save-hook #'jd/org-auto-tangle))
+
+
+
+
+
+
+;; debugging steps. Commenting these out now that it seems to be working.
+
+;; (message "DEBUG: About to load helpers...")
+;; (defvar te nil)
+
+;; (advice-add 'load :before (lambda (f &rest _) (message ">>> LOADING %s" f)))
+
+
+;; (defun trace-load (file &rest _)
+;;   (message ">>> LOADING %s" file))
+;; (advice-add 'load :before #'trace-load)
+
+
+;; https://github.com/daviwil/emacs-from-scratch/blob/master/show-notes/Emacs-Scratch-12.org
+
 ;; --------------------
 ;; Performance optimizations
 ;; --------------------
 
 
-(setq load-prefer-newer t)              ; do this once, near top of init
+
 
 ;; 2) --- use GCMH via use-package -----------------------
 (use-package gcmh
@@ -73,160 +108,43 @@
 
 
 (use-package wc-goal-mode
-  :defer t
+  :defer
   :ensure t
+  :defer
   :load-path "/Users/jay/emacs/emacs-settings/")
 
 (use-package counsel
-  :defer t
-  )
+  :defer)
 
 
 (autoload 'whittle "whittle" nil t)
 
-;; suppress auto evilifaction errors
-(with-eval-after-load 'evil-evilified-state
-  (add-to-list 'evil-evilified-state-modes 'org-agenda-mode))
 
 
-;; Automatically tangle config files
-
-
-
-
-(defun my/auto-tangle-org-file ()
-  "Automatically tangle and byte-compile org file on save"
-  (when (and (eq major-mode 'org-mode)
-             (string-match-p "/emacs-settings/" (buffer-file-name)))
-    (message ">>> AUTO-TANGLE running for %s" (buffer-name))
-    (require 'ob-tangle)
-    (let* ((org-file (buffer-file-name))
-           (el-file (concat (file-name-sans-extension org-file) ".el"))
-           (elc-file (concat el-file "c")))
-      (message "DEBUG: Tangling %s to %s" org-file el-file)
-
-      ;; Use the same tangle method as your load function
-      (org-babel-tangle-file org-file el-file "emacs-lisp")
-
-      (message "DEBUG: After tangling, .el file exists: %s" (file-exists-p el-file))
-
-      (when (file-exists-p el-file)
-        (message "DEBUG: Compiling %s" el-file)
-        (byte-compile-file el-file)
-        (when (get-buffer "*Compile-Log*")
-          (kill-buffer "*Compile-Log*"))
-        (message "Tangled and compiled: %s" (file-name-nondirectory el-file))))))
-
-(add-hook 'after-save-hook #'my/auto-tangle-org-file)
-
-
-;;; -----------------------------------------------------------
-;;; helpers ----------------------------------------------------
-(defun my/native-comp-available-p ()
-  "Return non-nil only when the native compiler *really* works."
-  (and (fboundp 'native-compile)          ; symbol exists
-       (require 'comp nil t)             ; library is loaded (≥ 28)
-       (functionp 'native-comp-available-p)
-       (native-comp-available-p)))       ; linked with libgccjit
-
-(defun my/compiled-path (el-file)
-  "Best existing artefact for EL-FILE: .eln ▸ .elc ▸ EL-FILE."
-  (let ((eln (and (fboundp 'native-compiled-file)
-                  (native-compiled-file el-file)))
-        (elc (byte-compile-dest-file el-file)))
-    (cond ((and eln (file-exists-p eln)) eln)
-          ((file-exists-p elc)           elc)
-          (t                             el-file))))
-
-
-
-(defun my/load-org-config (org-file)
-  "Tangle ORG-FILE → *.el, compile if needed, then load it."
-  (require 'ob-tangle) ; Ensure tangling functions are available
-  (let* ((org-file   (expand-file-name org-file))
-         (el-file    (concat (file-name-sans-extension org-file) ".el"))
-         ;; Get the path to the best *existing* compiled artifact or the .el file
-         (existing-bin (my/compiled-path el-file)))
-
-    ;; Fast path: if a compiled version exists and it's newer than the .org source, just load it.
-    ;; `existing-bin` could be .eln, .elc, or .el itself if nothing compiled yet.
-    ;; We only take the fast path if `existing-bin` is NOT the .el file (i.e., it's a compiled version)
-    ;; AND this compiled version is up-to-date with respect to the .org file.
-    (if (and existing-bin
-             (not (string= el-file existing-bin)) ; Ensure 'existing-bin' is a compiled file, not the .el itself
-             (file-exists-p existing-bin)
-             (file-newer-than-file-p existing-bin org-file))
-        (progn
-          ;; (message "Fast path: Loading pre-compiled %s (for %s)" existing-bin org-file)
-          (load (file-name-sans-extension el-file) nil 'nomessage))
-
-        ;; Slow path: (Re)tangle and/or (re)compile, then load.
-        (progn
-          ;; (message "Slow path: Processing %s" org-file)
-          ;; 1 ─ Tangle if .el is missing or .org is newer
-          (when (or (not (file-exists-p el-file))
-                    (file-newer-than-file-p org-file el-file))
-            ;; (message "Tangling %s -> %s" org-file el-file)
-            (org-babel-tangle-file org-file el-file "emacs-lisp"))
-
-          ;; Proceed only if tangling produced the .el file
-          (when (file-exists-p el-file)
-            ;; Update `bin` to reflect the best compiled version *after* potential re-tangle/re-compile
-            ;; This check is for the state *before* we potentially compile *now*.
-            (let ((current-compiled-artifact (my/compiled-path el-file)))
-              ;; 2 ─ (Re)compile if .el is newer than its current compiled artifact,
-              ;;     or if the compiled artifact doesn't exist (e.g. current-compiled-artifact is el-file itself).
-              (when (or (not (file-exists-p current-compiled-artifact))
-                        (string= el-file current-compiled-artifact) ; .el file is the "bin", so it needs compilation
-                        (file-newer-than-file-p el-file current-compiled-artifact))
-                ;; (message "Compiling %s..." el-file)
-                (if (my/native-comp-available-p)
-                    (native-compile el-file)
-                    (byte-compile-file el-file))
-                ;; 3 ─ Hide *Compile-Log*
-                (when-let ((buf (get-buffer "*Compile-Log*"))) (kill-buffer buf))))
-
-            ;; 4 ─ Load the code
-            ;; (message "Loading (after potential tangle/compile) %s" (file-name-sans-extension el-file))
-            (load (file-name-sans-extension el-file) nil 'nomessage))))))
-
-;;; -----------------------------------------------------------
-
-(dolist (f '("~/emacs/emacs-settings/gnu-emacs-startup.org"
-             "~/emacs/emacs-settings/shared-functions.org"
-             "~/emacs/emacs-settings/spacecraft-mode.org"
-             "~/emacs/emacs-settings/pasteboard-copy-and-paste-functions.org"
-             "~/emacs/emacs-settings/search-commands.org"
-             "~/emacs/emacs-settings/fonts-and-themes.org"))
-  (my/load-org-config f))
 
 
 
 
 (load "/Users/jay/emacs/emacs-settings/jay-osx.el")
-(load "/Users/jay/emacs/external-packages/prelude/core/prelude-core.el")
-
-(autoload 'prelude-core "prelude-core" nil t)
-
-(load "/Users/jay/emacs/emacs-settings/skeletons.el")
-
-(autoload 'prelude-key-chord "prelude-key-chord" nil t)
-
-
-(load "/Users/jay/emacs/emacs-settings/org-roam-config.el")
-
-
-;; (load "/Users/jay/emacs/emacs-settings/poetry_JD.el")
-
-;; (load "/Users/jay/gnulisp/book-functions.el")
+(org-babel-load-file "~/emacs/emacs-settings/gnu-emacs-startup.org")
+(org-babel-load-file "~/emacs/emacs-settings/shared-functions.org")
+(org-babel-load-file "~/emacs/emacs-settings/spacecraft-mode.org")
+(org-babel-load-file "~/emacs/emacs-settings/pasteboard-copy-and-paste-functions.org")
+(org-babel-load-file "/Users/jay/emacs/emacs-settings/search-commands.org")
+(org-babel-load-file "/Users/jay/emacs/emacs-settings/fonts-and-themes.org")
 ;; (load "/Users/jay/emacs/archive/email.el")
 ;; (org-babel-load-file "/Users/jay/emacs/external-packages/org-mime-stuff/org-mime-stuff.org")
-
+(load "/Users/jay/emacs/external-packages/prelude/core/prelude-core.el")
+(load "/Users/jay/emacs/emacs-settings/skeletons.el")
+(load "/Users/jay/emacs/emacs-settings/prelude-key-chord.el")
+;; (load "/Users/jay/gnulisp/book-functions.el")
+(load "/Users/jay/emacs/emacs-settings/poetry_JD.el")
 ;; (load "/Users/jay/emacs/emacs-settings/define-word.el")
 ;; (load "/Users/jay/emacs/emacs-settings/searchlink/searchlink-new.el")
 ;; (load "/Users/jay/emacs/emacs-settings/ivy-smex.el")
 ;; (load "/Users/jay/emacs/emacs-settings/emacs_friends.el")
 ;; (load "/Users/jay/gnulisp/org-image.el")
+(load "/Users/jay/emacs/emacs-settings/org-roam-config.el")
 ;;(load "/Users/jay/emacs/emacs-settings/org-roam-review.el")
 
 ;; (monaco-font)
@@ -295,7 +213,7 @@
 ;; (use-package rg)
 
 (use-package reveal-in-finder
-  :defer t
+  :defer
   )
 
 ;; (recenter-top-bottom)
@@ -318,17 +236,9 @@
             (smartparens-global-mode 1)
             ))
 
+(toggle-fullscreen)
 
-;;; --- Full-screen at start-up ---------------------------------------------
-(defun jd/enter-fullscreen-once ()
-  "Make the initial GUI frame full-screen, then remove the hook."
-  (when (and (display-graphic-p)      ; skip if running in a tty
-             (fboundp 'toggle-frame-fullscreen))  ; built-in since Emacs 24
-    (toggle-frame-fullscreen))
-  ;; Only need to run once:
-  (remove-hook 'window-setup-hook #'jd/enter-fullscreen-once))
 
-(add-hook 'window-setup-hook #'jd/enter-fullscreen-once)
 
 
 ;; (electric-pair-mode 1)
@@ -391,6 +301,9 @@
 
 
 
+;; To permanently enable mode line display of org clock, add this snippet to your dotspacemacs/user-config function:
+;; (setq spaceline-org-clock-p t)
+
 (setq user-init-file "/Users/jay/emacs/emacs-settings/spacemacs.d/init.el")
 
 (setq case-fold-search t)
@@ -410,7 +323,7 @@
 ;; (server-reflash)
 ;;  (triplicate-code)
 ;; (embiggen-text)
-
+(scrollbar-init)
 
 ;; (setq max-lisp-eval-depth 10000)
 ;;; When opening a file that is a symbolic link, don't ask whether I
@@ -428,9 +341,9 @@
 
 
 (use-package server
-  )
+  :defer)
 (when (server-running-p)
   (server-force-delete))
-(server-start)
+;; (server-start)
 
 (redbold)
