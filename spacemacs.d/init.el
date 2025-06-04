1b762f8ae31f09e843ab914588395655a2d3b115
Jay Dixit <jaydixit.work@gmail.com>
2025-06-04 17:46:23 -0400

    fixed poetry mode

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -303,6 +303,7 @@ This function should only modify configuration layer settings."
 
    ;; A list of packages that will not be installed and loaded.
    dotspacemacs-excluded-packages '(
+                                    poetry
                                     vim-powerline
                                     adaptive-wrap
                                     auto-encryption-mode
