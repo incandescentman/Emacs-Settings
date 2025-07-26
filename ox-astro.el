a92b3f409ef83273355877f6e2117e2a6c9e9c75
Jay Dixit <jaydixit.work@gmail.com>
2025-07-26 01:17:16 -0400

    ox-astro

--- ox-astro.el
+++ ox-astro.el
@@ -1,7 +1,7 @@
 ;;; ox-astro.el --- Astro MDX Back-End for Org Export Engine  -*- lexical-binding: t -*-
 
-;; Author: Gemini
-;; Version: 0.4.0
+;; Author: Gemini & Jay Dixit
+;; Version: 0.5.2
 ;; Package-Requires: ((emacs "26.3"))
 ;; Keywords: Org, markdown, docs, astro
 ;; URL: https://github.com/your-repo/ox-astro
@@ -40,20 +40,6 @@
 ;; A single Org file exports to a single .mdx file. If #+POSTS_FOLDER is not
 ;; set, files are exported to a subdirectory named "astro-posts". This can
 ;; be customized via `org-astro-default-posts-folder`.
-;;
-;; # Supported Org Keywords
-;;
-;; The exporter recognizes the following keywords:
-;;
-;;   - #+title: The title of the post.
-;;   - #+author: The author's name.
-;;   - #+date / #+publish_date: The publication date.
-;;   - #+excerpt: A short summary of the post.
-;;   - #+cover_image: Path to the post's hero image.
-;;   - #+cover_image_alt: Alt text for the cover image.
-;;   - #+tags: Comma or space-separated list of tags.
-;;   - #+posts_folder: The absolute or relative path to the export directory.
-;;   - #+astro_imports: Raw import statements for MDX components.
 
 ;;; Code:
 
@@ -97,10 +83,19 @@ produce an ISO 8601 compliant date string, as is common in Astro."
 Handles both Org timestamps and plain 'YYYY-MM-DD' strings.
 INFO is the export options plist."
   (let ((date-fmt (plist-get info :astro-date-format)))
-    (format-time-string date-fmt
-                        (if (string-match-p org-ts-regexp-both date-raw)
-                            (org-time-string-to-time date-raw)
-                            (apply 'encode-time (parse-time-string date-raw))))))
+    (format-time-string
+     date-fmt
+     (if (string-match-p org-ts-regexp-both date-raw)
+         (org-time-string-to-time date-raw)
+         (let* ((parsed (parse-time-string date-raw))
+                (sec    (or (nth 0 parsed) 0))
+                (min    (or (nth 1 parsed) 0))
+                (hour   (or (nth 2 parsed) 0))
+                (day    (nth 3 parsed))
+                (mon    (nth 4 parsed))
+                (year   (nth 5 parsed))
+                (zone   (nth 8 parsed)))
+           (encode-time sec min hour day mon year zone))))))
 
 (defun org-astro--gen-yaml-front-matter (data)
   "Generate a YAML front-matter string from an alist DATA."
@@ -116,145 +111,167 @@ INFO is the export options plist."
                             (format "%s: " (symbol-name key))
                             (cond
                              ((listp val) ; For tags
-                              (concat "\n" (mapconcat (lambda (item) (format " - %s" item)) val "\n")))
+                              (concat "\n"
+                                      (mapconcat
+                                       (lambda (item) (format " - %s" item))
+                                       val "\n")))
                              ((stringp val)
                               ;; Don't quote image path if it starts with ~ or /
-                              (if (and (eq key 'image) (string-match-p "^[~/]" val))
+                              (if (and (memq key '(image imageAlt))
+                                       (string-match-p "^[~/]" val))
                                   (format "%s\n" val)
-                                  (format "\"%s\"\n" (replace-regexp-in-string "\"" "\\\\\"" val))))
-                             (t (format "%s\n" val)))))))
-          (concat yaml-str "---\n"))))
+                                  (format "\"%s\"\n"
+                                          (replace-regexp-in-string "\"" "\\\\\"" val))))
+                             (t
+                              (format "%s\n" val))))))))
+        ;; close front-matter block
+        (concat yaml-str "---\n"))))
 
-  (defun org-astro--get-front-matter (info)
-    "Return the Astro front-matter string.
+(defun org-astro--get-front-matter (info)
+  "Return the Astro front-matter string.
 INFO is a plist used as a communication channel."
-    (let* ((title (plist-get info :title))
-           (author (plist-get info :author))
-           (excerpt (or (plist-get info :astro-excerpt) (plist-get info :excerpt)))
-           (image (or (plist-get info :astro-image) (plist-get info :cover-image)))
-           (image-alt (or (plist-get info :astro-image-alt) (plist-get info :cover-image-alt)))
-           (tags-raw (or (plist-get info :astro-tags) (plist-get info :tags)))
-           (tags (when tags-raw (org-split-string tags-raw "[, ]+")))
-           (publish-date
-            (or (plist-get info :astro-publish-date)
-                (let ((date-raw (or (plist-get info :publish-date) (plist-get info :date))))
-                  (when date-raw (org-astro--format-date date-raw info)))))
-           (data `((title . ,title)
-                   (author . ,author)
-                   (publishDate . ,publish-date)
-                   (excerpt . ,excerpt)
-                   (image . ,image)
-                   (imageAlt . ,image-alt)
-                   (tags . ,tags))))
-      (org-astro--gen-yaml-front-matter data)))
-
+  (let* ((title (plist-get info :title))
+         (author (plist-get info :author))
+         (excerpt (or (plist-get info :astro-excerpt)
+                      (plist-get info :excerpt)))
+         (image (or (plist-get info :astro-image)
+                    (plist-get info :cover-image)))
+         (image-alt (or (plist-get info :astro-image-alt)
+                        (plist-get info :cover-image-alt)))
+         (tags-raw (or (plist-get info :astro-tags)
+                       (plist-get info :tags)))
+         (tags (when tags-raw
+                 (org-split-string tags-raw "[, \n]+")))
+         (publish-date (or (plist-get info :astro-publish-date)
+                           (let ((date-raw (or (plist-get info :publish-date)
+                                               (plist-get info :date))))
+                             (when date-raw
+                               (org-astro--format-date date-raw info))))))
+    (org-astro--gen-yaml-front-matter
+     `((title . ,title)
+       (author . ,author)
+       (publishDate . ,publish-date)
+       (excerpt . ,excerpt)
+       (image . ,image)
+       (imageAlt . ,image-alt)
+       (tags . ,tags)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Transcode Functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-  (defun org-astro-link (link desc info)
-    "Transcode a LINK object for Astro MDX.
+(defun org-astro-link (link desc info)
+  "Transcode a LINK object for Astro MDX.
 Handles internal links to headings by creating anchor links."
-    (let ((type (org-element-property :type link))
-          (path (org-element-property :path link)))
-      (if (and (string= type "fuzzy") (not (string-match-p "://" path)))
-          ;; This is likely an internal link like [[Introduction]]
-          (let* ((target (org-export-resolve-fuzzy-link link info))
-                 (title (org-element-property :raw-value target))
-                 (slug (org-astro--slugify title)))
-            (format "[%s](#%s)" (or desc title) slug))
-          ;; Fallback to the default markdown link handler for other types
-          (org-md-link link desc info))))
+  (let ((type (org-element-property :type link))
+        (path (org-element-property :path link)))
+    (if (and (string= type "fuzzy")
+             (not (string-match-p "://" path)))
+        ;; Internal fuzzy link
+        (let* ((target (org-export-resolve-fuzzy-link link info))
+               (title (org-element-property :raw-value target))
+               (slug (org-astro--slugify title)))
+          (format "[%s](#%s)" (or desc title) slug))
+        ;; fallback to default
+        (org-md-link link desc info))))
 
-  (defun org-astro-heading (heading contents info)
-    "Transcode a HEADING element, adding an ID for anchor links."
-    (let* ((title (org-export-data (org-element-property :title heading) info))
-           (slug (org-astro--slugify title))
-           (level (+ (org-element-property :level heading)
-                     (or (plist-get info :headline-offset) 0)))
-           (headline (format "%s %s" (make-string level ?#) title)))
-      (format "%s {#%s}\n\n%s" headline slug (or contents ""))))
-
+(defun org-astro-heading (heading contents info)
+  "Transcode a HEADING element, adding an ID for anchor links."
+  (let* ((title (org-export-data (org-element-property :title heading) info))
+         (slug (org-astro--slugify title))
+         (level (+ (org-element-property :level heading)
+                   (or (plist-get info :headline-offset) 0)))
+         (header (format "%s %s" (make-string level ?#) title)))
+    (format "%s {#%s}\n\n%s" header slug (or contents ""))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Filter Functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-  (defun org-astro-body-filter (body _backend info)
-    "Add front-matter and imports to the BODY of the document."
-    (let ((front-matter (org-astro--get-front-matter info))
-          (imports (or (plist-get info :astro-imports) "")))
-      (concat front-matter
-              (if (string-blank-p imports) "" (concat imports "\n\n"))
-              body)))
+(defun org-astro-body-filter (body _backend info)
+  "Add front-matter and imports to the BODY of the document."
+  (let ((front-matter (org-astro--get-front-matter info))
+        (imports (or (plist-get info :astro-imports) "")))
+    (concat front-matter
+            (if (string-blank-p imports)
+                ""
+                (concat imports "\n\n"))
+            body)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Main Export Functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;###autoload
-  (defun org-astro-export-as-mdx (&optional async)
-    "Export current buffer to an Astro-compatible MDX buffer."
-    (interactive)
-    (org-export-to-buffer 'astro "*Astro MDX Export*" async))
+(defun org-astro-export-as-mdx (&optional async)
+  "Export current buffer to an Astro-compatible MDX buffer."
+  (interactive)
+  (org-export-to-buffer 'astro "*Astro MDX Export*" async))
 
 ;;;###autoload
-  (defun org-astro-export-to-mdx (&optional async)
-    "Export current buffer to an Astro-compatible MDX file.
+(defun org-astro-export-to-mdx (&optional async)
+  "Export current buffer to an Astro-compatible MDX file.
 If a POSTS_FOLDER keyword is present, export to that directory.
 Otherwise, export to the directory specified by
 `org-astro-default-posts-folder'."
-    (interactive)
-    (let* ((info (org-export-get-environment 'astro))
-           (posts-folder (or (plist-get info :astro-posts-folder)
-                             (plist-get info :posts-folder)))
-           (pub-dir
-            (if posts-folder
-                (file-name-as-directory (expand-file-name (org-trim posts-folder)))
-                (let ((default-astro-dir
-                       (file-name-as-directory
-                        (expand-file-name org-astro-default-posts-folder default-directory))))
-                  (make-directory default-astro-dir :parents)
-                  default-astro-dir)))
-           (outfile (org-export-output-file-name ".mdx" nil pub-dir)))
-      (org-export-to-file 'astro outfile async)))
+  (interactive)
+  (let* ((info (org-export-get-environment 'astro))
+         (posts-folder (or (plist-get info :astro-posts-folder)
+                           (plist-get info :posts-folder)))
+         (pub-dir (if posts-folder
+                      (file-name-as-directory
+                       (expand-file-name (org-trim posts-folder)))
+                      (let ((d (file-name-as-directory
+                                (expand-file-name org-astro-default-posts-folder
+                                                  default-directory))))
+                        (make-directory d :parents)
+                        d)))
+         (outfile (org-export-output-file-name ".mdx" nil pub-dir)))
+    (org-export-to-file 'astro outfile async)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Backend Definition
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-  (org-export-define-derived-backend 'astro 'md
-    :menu-entry
-    '(?a "Export to Astro"
-         ((?X "As MDX buffer" org-astro-export-as-mdx)
-          (?x "To MDX file" org-astro-export-to-mdx)
-          (?o "To MDX file and open"
-              (lambda (_a _s _v _b) (org-open-file (org-astro-export-to-mdx))))))
-    :translate-alist '((link . org-astro-link)
-                       (headline . org-astro-heading))
-    :filters-alist '((:filter-body . org-astro-body-filter))
-    ;; Define the keywords this backend will recognize from the Org file.
-    :options-alist '(
-                     (:title "TITLE" nil)
-                     (:author "AUTHOR" nil)
-                     (:date "DATE" nil)
-                     (:publish-date "PUBLISH_DATE" nil)
-                     (:excerpt "EXCERPT" nil)
-                     (:tags "TAGS" nil newline)
-                     (:cover-image "COVER_IMAGE" nil)
-                     (:cover-image-alt "COVER_IMAGE_ALT" nil)
-                     (:posts-folder "POSTS_FOLDER" nil)
-                     (:astro-publish-date "ASTRO_PUBLISH_DATE" nil)
-                     (:astro-excerpt "ASTRO_EXCERPT" nil)
-                     (:astro-image "ASTRO_IMAGE" nil)
-                     (:astro-image-alt "ASTRO_IMAGE_ALT" nil)
-                     (:astro-tags "ASTRO_TAGS" nil newline)
-                     (:astro-imports "ASTRO_IMPORTS" nil newline)
-                     (:astro-posts-folder "ASTRO_POSTS_FOLDER" nil)
-                     (:astro-date-format nil "date-format" org-astro-date-format)
-                     ))
+(org-export-define-derived-backend 'astro 'md
+  :menu-entry
+  '(?a "Export to Astro"
+       ((?X "As MDX buffer" org-astro-export-as-mdx)
+        (?x "To MDX file" org-astro-export-to-mdx)
+        (?o "To MDX file and open"
+            (lambda (_a _s _v _b)
+              (org-open-file
+               (org-astro-export-to-mdx))))))
 
-  (provide 'ox-astro)
+  :translate-alist
+  '((link . org-astro-link)
+    (headline . org-astro-heading))
+
+  :filters-alist
+  '((:filter-body . org-astro-body-filter))
+
+  ;; The format for each entry is:
+  ;; (PLIST-KEYWORD ORG-KEYWORD OPTION-KEY DEFAULT-VAR BEHAVIOR-SYMBOL)
+  ;; Most are nil, but the structure must be correct.
+  :options-alist
+  '((:title              "TITLE"                nil nil nil)
+    (:author             "AUTHOR"               nil nil nil)
+    (:date               "DATE"                 nil nil nil)
+    (:publish-date       "PUBLISH_DATE"         nil nil nil)
+    (:excerpt            "EXCERPT"              nil nil nil)
+    (:tags               "TAGS"                 nil nil 'newline)
+    (:cover-image        "COVER_IMAGE"          nil nil nil)
+    (:cover-image-alt    "COVER_IMAGE_ALT"      nil nil nil)
+    (:posts-folder       "POSTS_FOLDER"         nil nil nil)
+    (:astro-publish-date "ASTRO_PUBLISH_DATE"   nil nil nil)
+    (:astro-excerpt      "ASTRO_EXCERPT"        nil nil nil)
+    (:astro-image        "ASTRO_IMAGE"          nil nil nil)
+    (:astro-image-alt    "ASTRO_IMAGE_ALT"      nil nil nil)
+    (:astro-tags         "ASTRO_TAGS"           nil nil 'newline)
+    (:astro-imports      "ASTRO_IMPORTS"        nil nil 'newline)
+    (:astro-posts-folder "ASTRO_POSTS_FOLDER"   nil nil nil)
+    (:astro-date-format  nil "date-format" org-astro-date-format nil)))
+
+(provide 'ox-astro)
 
 ;;; ox-astro.el ends here
