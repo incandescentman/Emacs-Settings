b5413e3111ee82a314b2ce16d7468845e69c3dbf
Jay Dixit <jaydixit.work@gmail.com>
2025-09-19 07:18:13 +0900

    latex subtitles

--- latex-subtitles.el
+++ latex-subtitles.el
@@ -1,35 +1,15 @@
-(defun my/org-latex-process-subtitles (tree backend info)
-  "Process ~ lines as subtitles in LaTeX export."
-  (when (org-export-derived-backend-p backend 'latex)
-    (org-element-map tree 'headline
-      (lambda (headline)
-        (let ((contents (org-element-contents headline)))
-          (when contents
-            (let ((first-elem (car contents)))
-              (when (and (eq (org-element-type first-elem) 'paragraph)
-                         (string-match "^~ \\(.+\\)"
-                                       (org-element-interpret-data first-elem)))
-                (let ((subtitle-text (match-string 1
-                                                   (org-element-interpret-data first-elem))))
-                  (org-element-set-contents first-elem
-                                            (concat "\\begin{subtitle}\n"
-                                                    subtitle-text
-                                                    "\n\\end{subtitle}")))))))
-        nil)))
-  tree)
-
-
 (defun my-org-latex-subtitle-filter (contents backend info)
   "Convert lines starting with ~ to subtitle environment in LaTeX export."
   (when (eq backend 'latex)
-    (replace-regexp-in-string
-     "^\\s-*~\\s-*\\(.+\\)$"
-     "\\\\begin{subtitle}\n\\1\n\\\\end{subtitle}"
-     contents)))
+    ;; Replace the escaped tilde pattern with subtitle environment
+    (setq contents
+          (replace-regexp-in-string
+           "\\\\textasciitilde{}[[:space:]]*\\(.+\\)"
+           "\\\\begin{subtitle}\n\\1\n\\\\end{subtitle}"
+           contents)))
+  contents)
 
-(add-to-list 'org-export-filter-final-output-functions
-             'my-org-latex-subtitle-filter)
-
-
-(add-to-list 'org-export-filter-parse-tree-functions
-             'my/org-latex-process-subtitles)
+;; Add the filter to final output
+(with-eval-after-load 'ox
+  (add-to-list 'org-export-filter-final-output-functions
+               'my-org-latex-subtitle-filter))
