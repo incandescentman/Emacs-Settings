eaa4ba802514489f861c92dd166da3a98dc2f1c3
Jay Dixit <jaydixit.work@gmail.com>
2026-08-13 18:08:12 -0400

    Improve Org checkbox completion workflow

--- keys.el
+++ keys.el
@@ -175,7 +175,7 @@
     ("s-d"           . org-todo)
     ("M-d"           . org-todo)
     ("M-s-9"         . org-todo)
-    ("s-j"           . org-todo)
+    ("s-j"           . jay/org-checkbox-toggle-and-advance) ; checkbox → tick + advance; else org-todo
     ("s-k o l"       . olivetti-mode)
     ("] ol"          . olivetti-mode)
     ("s-k o e"       . olivetti-expand)
@@ -764,32 +764,7 @@ Used by `my/keybinding-dump' to generate documentation.")
 (with-eval-after-load 'org
   (my/unbind-keys  org-mode-map my/org-mode-unbindings)
   (my/install-mode-bindings org-mode-map my/org-mode-bindings)
-  (my/install-mode-bindings key-minor-mode-map my/org-override-bindings)
-
-  ;; Auto-advance to next checkbox after toggling with C-c C-c
-  (defmacro my/with-advice (adlist &rest body)
-    "Execute BODY with temporary advice in ADLIST.
-Each element of ADLIST should look like (FUNCTION WHERE AD-FN)."
-    (declare (debug ((&rest (&rest form)) body))
-             (indent 1))
-    `(progn
-       ,@(mapcar (lambda (adform)
-                   `(advice-add ',(car adform) ,(cadr adform) ,(caddr adform)))
-                 adlist)
-       (unwind-protect (progn ,@body)
-         ,@(mapcar (lambda (adform)
-                     `(advice-remove ',(car adform) ,(caddr adform)))
-                   adlist))))
-
-  (defun my/org-checkbox-toggle-advice (orig-fn &rest args)
-    "Advice to move to next list item after checkbox toggle."
-    (my/with-advice
-        ((org-update-checkbox-count-maybe
-          :after (lambda () (ignore-errors (org-next-item)))))
-      (apply orig-fn args)))
-
-  (advice-add #'org-ctrl-c-ctrl-c   :around #'my/org-checkbox-toggle-advice)
-  (advice-add #'org-toggle-checkbox :around #'my/org-checkbox-toggle-advice))
+  (my/install-mode-bindings key-minor-mode-map my/org-override-bindings))
 
 (with-eval-after-load 'org-src
   (my/install-mode-bindings org-src-mode-map my/org-src-mode-bindings))
